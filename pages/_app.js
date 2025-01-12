102ef0d21c5a7a46263aac7d760e14e4cfeafa31
mishiningo <lbutschowitz@gmail.com>
2025-01-12 18:07:54 +0000

    reversão

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <Layout>
-      <Head title={`Leonardo Butschowitz | ${pageProps.title}`} />
+      <Head title={`Nitin Ranganath | ${pageProps.title}`} />
       <Component {...pageProps} />
     </Layout>
   );
